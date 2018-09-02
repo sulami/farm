5f6842fe2627009e1af32d0fc21da8f4e688a258
Robin Schroer <sulami@peerwire.org>
2018-09-02 14:37:05 +0100

    You can buy seeds

--- src/cljs/farm/core.cljs
+++ src/cljs/farm/core.cljs
@@ -3,16 +3,52 @@
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]))
 
+(defonce status
+  (atom {:money 1000
+         :seeds 0}))
+
+;; -------------------------
+;; Game functions
+
+(defn buy-seeds []
+  (swap!
+   status
+   (fn [current]
+     (let [new-money (-> current :money (- 50))
+           new-seeds (-> current :seeds (+ 200))]
+       (into current
+             {:money new-money
+              :seeds new-seeds})))))
+
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to farm"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+  [:div
+   [:h2 "Welcome to the Farm"]
+   [:ul
+    [:li [:a {:href "/game"} "Start farming"]]
+    [:li [:a {:href "/about"} "About"]]]])
 
 (defn about-page []
-  [:div [:h2 "About farm"]
-   [:div [:a {:href "/"} "go to the home page"]]])
+  [:div [:h2 "About Farm"]
+   [:p "The Farm is all about farming. "
+    [:a {:href "/game"} "So start farming."]]])
+
+(defn game-page []
+  [:div [:h2 "Farm"]
+
+   ;; Status
+   [:div
+    [:p (str "Money: $" (-> @status :money))]
+    [:p (str "Seeds: " (-> @status :seeds))]]
+
+   ;; Actions
+   [:div
+    [:input {:type "button"
+             :value "Buy seeds"
+             :on-click buy-seeds}]]
+   ])
 
 ;; -------------------------
 ;; Routes
@@ -24,6 +60,9 @@
 
 (secretary/defroute "/" []
   (reset! page #'home-page))
+
+(secretary/defroute "/game" []
+  (reset! page #'game-page))
 
 (secretary/defroute "/about" []
   (reset! page #'about-page))
