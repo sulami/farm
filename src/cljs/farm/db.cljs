99ad45ee5a0e1df3422f1cfcfd097f304a386c54
Robin Schroer <sulami@peerwire.org>
2018-09-20 16:10:20 +0100

    Spec the db

--- src/cljs/farm/db.cljs
+++ src/cljs/farm/db.cljs
@@ -1,5 +1,60 @@
 (ns farm.db
-  (:require [farm.config :as config]))
+  (:require [cljs.spec.alpha :as s]
+            [farm.config :as config]
+            [farm.utils :refer [in?]]))
+
+
+;; Specs
+
+(s/def ::game-time
+  (s/and int? #(>= % 0)))
+
+(s/def ::age
+  (s/and int? #(>= % 0)))
+
+(s/def ::family-member
+  (s/keys :req-un [::name
+                   ::age]))
+
+(s/def ::family
+  (s/coll-of ::family-member))
+
+(s/def ::money int?)
+(s/def ::seed int?)
+(s/def ::seed-price int?)
+(s/def ::food int?)
+(s/def ::food-price int?)
+(s/def ::temperature float?)
+
+(s/def ::weather
+  #(in? [:sunny
+         :clear
+         :overcast
+         :rain
+         :hail
+         :thunderstorm] %))
+
+(s/def ::plant
+  (s/nilable
+   (s/keys :req-un [::age
+                    ::water])))
+
+(s/def ::plants
+  (s/coll-of ::plant))
+
+(s/def ::db
+  (s/keys :req-un [::game-time
+                   ::family
+                   ::money
+                   ::seed
+                   ::seed-price
+                   ::food
+                   ::food-price
+                   ::temperature
+                   ::weather
+                   ::plants]))
+
+;; Default DB
 
 (def default-db
   {:game-time 0
