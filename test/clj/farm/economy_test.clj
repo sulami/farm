49137fe44616652eebbecfbadd9fb5e8679b56c1
Robin Schroer <sulami@peerwire.org>
2018-09-26 15:37:52 +0200

    Test the rest of economy

--- test/clj/farm/economy_test.clj
+++ test/clj/farm/economy_test.clj
@@ -7,11 +7,43 @@
   (testing "Sanity"
     (is (= 1 1) "Insanity")))
 
+(deftest resource-price-key-test
+  (testing "is appending '-price'"
+    (is (= (resource-price-key :foo)
+           :foo-price))))
+
+;; Event Handler Tests
+
 (deftest consume-food-test
   (testing "food is reduced by number of family members"
     (let* [db (initialize-db {} [:initialize-db])
            family (-> db :family count)
            event [:consume-food]
            db' (consume-food db event)]
-      (is (= (-> db :food)
-             (-> db' :food (+ family)))))))
+      (is (= (-> db' :food (+ family))
+             (-> db :food))))))
+
+(deftest trade-resource-test
+  (let* [db (initialize-db {} [:initialize-db])
+         resource :food
+         resource-key (resource-price-key resource)
+         amount 10
+         price (-> db resource-key (* amount) (quot 10))]
+
+    (testing "buying"
+      (let [db' (trade-resource db [:trade-resource :buy resource amount])]
+        (testing "adds the right amount of resource"
+          (is (= (-> db' resource (- amount))
+                 (-> db resource))))
+        (testing "deducts the right amount of money"
+          (is (= (-> db' :money (+ price))
+                 (-> db :money))))))
+
+    (testing "selling"
+      (let [db' (trade-resource db [:trade-resource :sell resource amount])]
+        (testing "deducts the right amont of resource"
+          (is (= (-> db' resource (+ amount))
+                 (-> db resource))))
+        (testing "adds the right amount of money"
+          (is (= (-> db' :money (- price))
+                 (-> db :money))))))))
